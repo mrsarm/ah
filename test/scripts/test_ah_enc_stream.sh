8609df17bb80e0acd2ec41ff72f074884b4fcb87
Mariano Ruiz <mrsarm@gmail.com>
2021-09-18 21:05:10 -0300

    Add bash tests

---
+++ test/scripts/test_ah_enc_stream.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+AH="../out/ah"
+echo "Testing compressing and decompressing stream ..."
+echo -n "short message" | ${AH} -c | ${AH} -d | grep "short message" >/dev/null
+EXITCODE=$?
+test ${EXITCODE} -eq 0 && echo "... Testing compressing and decompressing stream done." \
+     || echo "... Testing compressing and decompressing stream failed with exit code ${EXITCODE}." >&2;
+test ${EXITCODE} -eq 0
---
+++ test/scripts/test_ah_wrong_args.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+AH="../out/ah"
+echo "Testing wrong arguments ..."
+${AH} -Nop >/dev/null 2>/dev/null
+EXITCODE=$?
+test ${EXITCODE} -eq 3 && echo "... Testing wrong arguments done." \
+     || echo "... Testing wrong arguments failed with exit code ${EXITCODE}." >&2;
+test ${EXITCODE} -eq 3
