a4003ed4a455a52f377de714036820e12a8b25e0
Mariano Ruiz <mrsarm@gmail.com>
2021-09-21 19:41:33 -0300

    Empty streams test cases

--- test/scripts/test_enc_stream.sh
+++ test/scripts/test_enc_stream.sh
@@ -2,7 +2,7 @@
 
 source "${BASH_SOURCE%/*}"/_setup_ah.sh
 echo "Testing compressing and decompressing stream ..."
-echo -n "short message" | ${AH} -c | ${AH} -d | grep "short message" >/dev/null
+echo -n "short message" | ${AH} -cv 2>/dev/null | ${AH} -dv 2>/dev/null | grep "short message" >/dev/null
 EXITCODE=$?
 test ${EXITCODE} -eq 0 && echo "... Testing compressing and decompressing stream done." \
      || echo "... Testing compressing and decompressing stream failed with exit code ${EXITCODE}." >&2;
