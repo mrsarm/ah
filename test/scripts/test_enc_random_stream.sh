35c4018de55360ce74031b5bb9112118e8432806
Mariano Ruiz <mrsarm@gmail.com>
2022-03-21 09:30:14 -0300

    Longer random number on tests

--- test/scripts/test_enc_random_stream.sh
+++ test/scripts/test_enc_random_stream.sh
@@ -2,7 +2,7 @@
 
 source "${BASH_SOURCE%/*}"/_setup_ah.sh
 echo "Testing compressing and decompressing random stream ..."
-RANDOM_MSG="${RANDOM}${RANDOM}${RANDOM}${RANDOM}"
+RANDOM_MSG="${RANDOM}${RANDOM}${RANDOM}${RANDOM}${RANDOM}${RANDOM}${RANDOM}${RANDOM}"
 echo -n ${RANDOM_MSG} | ${AH} -c | ${AH} -d | grep ${RANDOM_MSG} >/dev/null
 EXITCODE=$?
 test ${EXITCODE} -eq 0 && echo "... Testing compressing and decompressing random stream done." \
