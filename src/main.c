d96088317e7f3b34b15ab986bb0acf5fe8b93f60
Mariano Ruiz <mrsarm@gmail.com>
2022-07-21 18:45:57 -0300

    Bump → v3.0b1

--- src/main.c
+++ src/main.c
@@ -41,7 +41,7 @@
                 "  -h       display this help and exit\n" \
                 "\n" \
                 "With no FILE, or when FILE is -, read standard input.\n" \
-                "\"Another Huffman\" encoder project v2.0.0: ah <https://github.com/mrsarm/ah>\n"
+                "\"Another Huffman\" encoder project v3.0b1: ah <https://github.com/mrsarm/ah>\n"
 
 
 /* Initialize the global variables with the command arguments */
